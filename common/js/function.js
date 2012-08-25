15804e01efa0ecf532d866b967978c9767f3b390
h2ham <h2ham.mail@gmail.com>
2012-08-25 19:13:48 +0900

    デモ用

--- common/js/function.js
+++ common/js/function.js
@@ -1,3 +1,13 @@
-$('pre').on('click', function() {
-	$(this).find('span').hide();
+jQuery(function($) {
+
+  $('.key2').on('click', function() {
+    $(this).hide(function() {
+      $(this).next().show('slow');
+    });
+  });
+
+  $('pre').on('click', function() {
+    $(this).find('span').toggle();
+  });
+
 });
